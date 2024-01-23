235b5bd027e8b337a91ff625b58884241a2a7ee3
Hemram <kuheram777@gmail.com>
2024-01-23 13:27:11 +0530

    resolved errors

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -6,25 +6,6 @@ import { motion } from "framer-motion"
 import { fontItalic } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 
-export function CheckIcon() {
-  return (
-    <svg
-      className="mr-2 inline-block h-4 w-4"
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polyline points="20 6 9 17 4 12" />
-    </svg>
-  )
-}
-
 export default function() {
   return (
     <div className="dark:bg-gradient-to-r from-[#654120] to-[#2c6e31]">
